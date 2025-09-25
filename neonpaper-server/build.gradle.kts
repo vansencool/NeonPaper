4673f2d65b3942699db37660862e5b776d8f9f63
vansencool <vansencool@gmail.com>
2025-09-25 16:39:37 +0530

    build.gradle.kts

--- neonpaper-server/build.gradle.kts
+++ neonpaper-server/build.gradle.kts
@@ -175,7 +175,6 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-iostreams:2.24.1")
     implementation("org.ow2.asm:asm-commons:9.7.1")
     implementation("org.spongepowered:configurate-yaml:4.2.0-20250225.064233-199")
-    implementation("org.spongepowered:configurate-core:4.2.0-20250225.064233-204") // Pinned dependency of above pinned yaml snapshot.
     implementation("commons-lang:commons-lang:2.6")
     runtimeOnly("org.xerial:sqlite-jdbc:3.47.0.0")
     runtimeOnly("com.mysql:mysql-connector-j:9.1.0")
